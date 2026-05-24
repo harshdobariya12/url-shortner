18a248fb53e5697f2513d3e49dafae096848acaf
Harsh Dobariya <harshdobariya3838@gmail.com>
2026-05-24 10:49:13 +0530

    fix api base url

--- FRONTEND/src/api/user.api.js
+++ FRONTEND/src/api/user.api.js
@@ -1,26 +1,26 @@
 import axiosInstance from "../utils/axiosInstance"
 
 export const loginUser = async (password, email) => {
-    const {data} = await axiosInstance.post("/api/auth/login", {email, password})
+    const { data } = await axiosInstance.post("/api/auth/login", { email, password })
     return data
 }
 
 export const registerUser = async (name, password, email) => {
-    const {data} = await axiosInstance.post("/api/auth/register", {name, email, password})
+    const { data } = await axiosInstance.post("/api/auth/register", { name, email, password })
     return data
 }
 
 export const logoutUser = async () => {
-    const {data} = await axiosInstance.post("/api/auth/logout")
+    const { data } = await axiosInstance.post("/api/auth/logout")
     return data
 }
 
 export const getCurrentUser = async () => {
-    const {data} = await axiosInstance.get("/api/auth/me")
+    const { data } = await axiosInstance.get("/api/auth/me")
     return data
 }
 
 export const getAllUserUrls = async () => {
-    const {data} = await axiosInstance.get("/api/user/urls")
+    const { data } = await axiosInstance.get("/api/user/urls")
     return data
 }
