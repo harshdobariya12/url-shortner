589b5f43cc3ecb9cb8add784c411925e625cf38d
Harsh Dobariya <harshdobariya3838@gmail.com>
2026-05-24 11:38:34 +0530

    fix production api env

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -14,12 +14,12 @@ import cookieParser from "cookie-parser"
 dotenv.config("./.env")
 
 const app = express();
-const PORT =
 
-    app.use(cors({
-        origin: process.env.FRONTEND_URL || "http://localhost:5173", // your React app
-        credentials: true // 👈 this allows cookies to be sent
-    }));
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173", // your React app
+    credentials: true // 👈 this allows cookies to be sent
+}));
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
